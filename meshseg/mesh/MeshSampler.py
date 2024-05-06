9444e604b07a370e2f6fbc3c89a92c2648ab51b0
n-khalil <nader.khalil@ansys.com>
2024-05-06 22:17:03 +0200

    Convert point cloud to torch tensor in mesh sampler

--- meshseg/mesh/MeshSampler.py
+++ meshseg/mesh/MeshSampler.py
@@ -1,15 +1,18 @@
 import trimesh
 import numpy as np
+import torch
 
 class MeshSampler:
-    def __init__(self, mesh, n_samples_factor=2):
+    def __init__(self, mesh, device, n_samples_factor=2):
         self.mesh = mesh
         self.n_samples_factor = n_samples_factor
+        self.device = device
 
     def sample_mesh(self, n_samples):
         trimeshMesh = trimesh.Trimesh(self.mesh.vertices.cpu().numpy(), self.mesh.faces.cpu().numpy())
-        self.point_cloud = trimesh.sample.sample_surface_even(trimeshMesh, n_samples)
-        return self.point_cloud
+        point_cloud, pt_to_face = trimesh.sample.sample_surface_even(trimeshMesh, n_samples)
+        torchPC = torch.tensor(point_cloud, device=self.device, dtype=torch.float32)
+        return torchPC, pt_to_face
 
     def __call__(self):
         n_samples = int(self.mesh.vertices.shape[0] * self.n_samples_factor)
